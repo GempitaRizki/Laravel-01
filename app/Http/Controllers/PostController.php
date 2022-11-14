b173fe69e756f9c8b380d9227808f897c6be3248
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2022-11-14 10:25:41 +0700

    a little error in writing exception

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,12 +9,10 @@ class PostController extends Controller
 {
     public function index()
     {
-    //get posts
-    $posts = Post::latest()->paginate(5);
+        //get posts
+        $posts = Post::latest()->paginate(5);
 
-    //render view with posts 
-    return view('posts.index', compact('posts'));
+        //render view with posts
+        return view('index', compact('posts'));
     }
-
-
 }
