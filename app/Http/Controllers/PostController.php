0c80609ee7879e2786fd46faca9f4a9d392c4cbc
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2022-11-14 11:45:24 +0700

    create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,4 +15,32 @@ class PostController extends Controller
         //render view with posts
         return view('index', compact('posts'));
     }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+           //validate form
+           $this->validate($request, [
+            'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title'     => 'required|min:5',
+            'content'   => 'required|min:10'
+        ]); 
+
+            //upload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            //create post
+            Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content
+        ]);
+        //redirect to index
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Disimpan!']);
+    }
 }
